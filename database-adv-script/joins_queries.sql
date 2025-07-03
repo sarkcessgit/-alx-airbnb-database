c6d44c7a239be5203fe57bbc1ba7fe504c29ca9c
sarkcessgit <edomark664@gmail.com>
2025-07-03 08:48:26 -0400

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,4 +2,19 @@ SELECT b.*, u.*
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
 
+SELECT u.*, b.*
+FROM users u
+LEFT JOIN bookings b ON u.id = b.user_id
+UNION
+SELECT u.*, b.*
+FROM users u
+RIGHT JOIN bookings b ON u.id = b.user_id;
+
+SELECT u.*, b.*
+FROM users u
+LEFT JOIN bookings b ON u.id = b.user_id
+UNION
+SELECT u.*, b.*
+FROM users u
+RIGHT JOIN bookings b ON u.id = b.user_id;
 
