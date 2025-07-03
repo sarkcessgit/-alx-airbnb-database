60844789f7d2fd22c6b6d3e60a34a764b1fe6960
sarkcessgit <edomark664@gmail.com>
2025-07-03 09:09:27 -0400

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -5,3 +5,11 @@ WHERE id IN (
     FROM reviews
     GROUP BY property_id
     HAVING AVG(rating) > 4.0
+
+SELECT *
+FROM users u
+WHERE (
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.id
+) > 3;
