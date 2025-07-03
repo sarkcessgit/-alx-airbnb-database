57a7128d5bdaa42d0bc1236d4f42c614712b8efc
sarkcessgit <edomark664@gmail.com>
2025-07-03 09:42:08 -0400

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -6,3 +6,10 @@ CREATE INDEX idx_bookings_property_id ON bookings(property_id);
 
 -- Index to speed up ORDER BY queries on booking dates
 CREATE INDEX idx_bookings_created_at ON bookings(created_at);
+
+-- Use EXPLAIN ANALYZE to check performance of a query after indexing
+EXPLAIN ANALYZE
+SELECT * 
+FROM bookings 
+WHERE user_id = 42 
+ORDER BY created_at DESC;
